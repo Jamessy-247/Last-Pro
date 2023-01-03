89c43fd5240bba1647dc899ed2033ee08cc45f7b
Jamessy-247 <85467541+Jamessy-247@users.noreply.github.com>
2023-01-03 15:42:03 +0100

    Create Loading.js

---
+++ Loading.js
@@ -0,0 +1,17 @@
+// function showLoading() {
+//     document.getElementById('loading').style.display = 'block';
+//   }
+  
+//   function hideLoading() {
+//     document.getElementById('loading').style.display = 'none';
+//   }
+  
+//   showLoading()
+
+function showLoading() {
+    document.getElementById('loading').style.display = 'flex';
+  }
+  
+  function hideLoading() {
+    document.getElementById('loading').style.display = 'none';
+  }
